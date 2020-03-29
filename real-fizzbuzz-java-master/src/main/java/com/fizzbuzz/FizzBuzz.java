3f613043d9d4bc136e5a2005fd725d8678716df5
Bushy <bushy.netshidaulu@gmail.com>
2020-03-29 13:08:22 +0200

    refactoring and changing junit4 to 5

--- real-fizzbuzz-java-master/src/main/java/com/fizzbuzz/FizzBuzz.java
+++ real-fizzbuzz-java-master/src/main/java/com/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,7 @@ package com.fizzbuzz;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 
 import static java.util.Arrays.stream;
 import static java.util.function.Function.identity;
@@ -12,20 +13,20 @@ import static org.apache.commons.lang3.StringUtils.isNumeric;
 
 public class FizzBuzz {
 
-    public static String fizzBuzz(Integer number) {
-        return Optional.of(number).map(n -> {
+    public static Function<Integer, String> fizzBuzz = number -> {
+        Function<Integer, String> intToString = n -> {
             if (n.toString().contains("3")) return "lucky";
             else if (n % 15 == 0) return "fizzbuzz";
             else if (n % 3 == 0) return "fizz";
             else if (n % 5 == 0) return "buzz";
             else return n.toString();
-        }).get();
-    }
+        };
+        return Optional.of(number).map(intToString::apply).get();
+    };
+
 
     public static String realFizzBuzz(List<Integer> list) {
-        return list.stream()
-                .map(number -> fizzBuzz(number))
-                .collect(joining(" "));
+        return list.stream().map(fizzBuzz::apply).collect(joining(" "));
     }
 
     public static Map<String, Long> frequencies(String output) {
